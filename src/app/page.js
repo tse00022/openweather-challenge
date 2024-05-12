2b61f3a3d18ddb807f145b0038ce528ec166d597
Hiu Ming Tse <tse00022@algonquinlive.com>
2024-05-12 14:57:14 -0400

    Add search component

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,31 @@
 'use client'
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import Search from './Components/Search';
 
 export default function Page() {
 
+  const [location, setLocation] = useState();
+  const [backgroundImageURL, setBackgroundImageURL] = useState("01n");
+
+  const handle = (e) => {
+    setBackgroundImageURL(e);
+    console.log("backgroundImageURL:", e); // Output the value here
+  };
+
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <div>
-        <div>Hello Weather</div>
+    <div
+      className="mainpage"
+      style={{
+        backgroundImage: `url("./pics/${backgroundImageURL}.jpg")`,
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="searchComp">
+        <Search {...{ location, setLocation }} />
       </div>
+
+      {/* <Maindata city={location} setBackgroundImageURL={handle} /> */}
     </div>
   );
 }

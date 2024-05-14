9446121f95395576820c4c5ff43c175716fe25a6
Hiu Ming Tse <tse00022@algonquinlive.com>
2024-05-14 17:45:45 -0400

    Split startVoiceRecognition

--- src/app/Components/Dashboard.jsx
+++ src/app/Components/Dashboard.jsx
@@ -4,6 +4,7 @@ import moment from "moment";
 import LoadingPrompt from "./LoadingPrompt";
 import MicrophonePrompt from "./MicrophonePrompt";
 import { createModel, KaldiRecognizer } from "vosk-browser";
+import { startVoiceRecognition } from "../../utils/startVoiceRecognition";
 import * as fuzz from 'fuzzball';
 
 export default function Dashboard({ baseURL }) {
@@ -81,64 +82,8 @@ export default function Dashboard({ baseURL }) {
   }, [microphoneEnabled]);
 
   useEffect(() => {
-    const startVoiceRecognition = async () => {
-      if (recognizer) {
-        const mediaStream = await navigator.mediaDevices.getUserMedia({
-          video: false,
-          audio: {
-            echoCancellation: true,
-            noiseSuppression: true,
-            channelCount: 1,
-            sampleRate: 16000
-          },
-        });
-
-        const audioContext = new AudioContext();
-
-        // Define the AudioWorkletProcessor as a string
-        const processorCode = `
-          class RecognizerProcessor extends AudioWorkletProcessor {
-            process(inputs, outputs, parameters) {
-              const input = inputs[0];
-              if (input.length > 0) {
-                const inputBuffer = input[0];
-                const buffer = new Float32Array(inputBuffer.length);
-                inputBuffer.forEach((sample, index) => {
-                  buffer[index] = sample;
-                });
-                this.port.postMessage(buffer);
-              }
-              return true;
-            }
-          }
-          registerProcessor('recognizer-processor', RecognizerProcessor);
-        `;
-
-        // Create a Blob URL from the processor code
-        const blob = new Blob([processorCode], { type: 'application/javascript' });
-        const url = URL.createObjectURL(blob);
-
-        // Add the module to the AudioWorklet
-        await audioContext.audioWorklet.addModule(url);
-
-        const recognizerNode = new AudioWorkletNode(audioContext, 'recognizer-processor');
-        recognizerNode.port.onmessage = (event) => {
-          try {
-            const audioBuffer = audioContext.createBuffer(1, event.data.length, audioContext.sampleRate);
-            audioBuffer.copyToChannel(event.data, 0);
-            recognizer.acceptWaveform(audioBuffer);
-          } catch (error) {
-            console.error('acceptWaveform failed', error);
-          }
-        };
-
-        const source = audioContext.createMediaStreamSource(mediaStream);
-        source.connect(recognizerNode);
-      }
-    };
-
     if (recognizer) {
-      startVoiceRecognition();
+      startVoiceRecognition(recognizer);
     }
   }, [recognizer]);
 
